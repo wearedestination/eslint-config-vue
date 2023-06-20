2873dfc91931dec8021c767b6271a5d890d2042a
andyexeter <palmer.andy@gmail.com>
2023-06-20 13:30:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Destination ESLint Config Vue
+
+Defines a set of rules used by [ESLint](https://eslint.org/) to enforce our JS/TS coding standards for Vue projects
+
+## Installation
+
+```sh
+yarn add @destination/eslint-config-vue eslint --dev
+```
+
+## Usage
+
+Add the following to your project's ruleset e.g. in `package.json`:
+
+```json
+{
+  "eslint": {
+    "extends": "@destination/eslint-config-vue"
+  }
+}
+```
+
+Run `eslint` as normal e.g.:
+
+```sh
+yarn eslint 'assets/**/*.{js,ts,vue}'
+```
+
+## License
+
+Released under the [MIT license](LICENSE)
---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-recommended"
+    ],
+    "rules": {
+        "curly": "error",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "vue/html-indent": [
+            "error",
+            4
+        ],
+        "vue/html-self-closing": [
+            "error",
+            {
+                "html": {
+                    "void": "always",
+                    "normal": "never",
+                    "component": "always"
+                },
+                "svg": "always",
+                "math": "always"
+            }
+        ],
+        "vue/max-attributes-per-line": "off",
+        "vue/multi-word-component-names": "off",
+        "vue/no-v-html": "off",
+        "vue/singleline-html-element-content-newline": "off",
+    }
+}
